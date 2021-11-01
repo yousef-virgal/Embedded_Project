26e959917edc68644d9329992d5ccb91c67b845b
Yousef <yousefshafik234@yahoo.com>
2021-11-01 16:28:15 +0200

    added types.h file

---
+++ types.h
@@ -0,0 +1,30 @@
+typedef signed char int8;
+typedef unsigned char uint8;
+typedef signed char* int8_ptr;
+typedef unsigned char* uint8_ptr;
+
+typedef signed short int16;
+typedef unsigned short uint16;
+typedef signed short* int16_ptr;
+typedef unsigned short* uint16_ptr;
+
+typedef signed int int32;
+typedef unsigned int uint32;
+typedef signed int* int32_ptr;
+typedef unsigned int int32_ptr;
+
+typedef signed long int64;
+typedef unsigned long uint64;
+typedef signed long* int64_ptr;
+typedef unsigned long* uint64_ptr;
+
+typedef float float32;
+typedef float* float32_ptr;
+
+typedef double float64;
+typedef double* float64;
+
+typedef signed long double double64;
+typedef signed long double* double64_ptr;
+
+
